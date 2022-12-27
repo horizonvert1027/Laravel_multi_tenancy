9eb54e1833df060ddf80e4b1cc2d1aebccf49551
horizonvert98 <horizonvert.pu@gmail.com>
2022-12-27 04:37:38 +0200

    issue

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,18 @@
+
+<?php declare(strict_types=1);
+
+if (! function_exists('geo')) {
+    function geo(?string $countryCode = null)
+    {
+        if (is_null($countryCode)) {
+            return app('geo');
+        }
+
+        app('geo')->put($countryCode);
+    }
+}
+
+
 <?php declare(strict_types=1);
 
 if (! function_exists('geo')) {
