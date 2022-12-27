13be88667d8f643b0545d3a4387b608095a0a1e0
horizonvert98 <horizonvert.pu@gmail.com>
2022-12-27 04:42:31 +0200

    recover

--- src/helpers.php
+++ src/helpers.php
@@ -13,15 +13,3 @@ if (! function_exists('geo')) {
 }
 
 
-<?php declare(strict_types=1);
-
-if (! function_exists('geo')) {
-    function geo(?string $countryCode = null)
-    {
-        if (is_null($countryCode)) {
-            return app('geo');
-        }
-
-        app('geo')->put($countryCode);
-    }
-}
